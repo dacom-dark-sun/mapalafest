a271d95fe9811475d2730b6c923b7a7bb4bd7438
Dark Sun <dacom.dark.sun@gmail.com>
2017-08-22 13:10:23 +0500

    payment

--- payment.php
+++ payment.php
@@ -4,22 +4,107 @@ date_default_timezone_set ("UTC");
 define ("MAIN_DIR", dirname(__FILE__));
 include (MAIN_DIR . '/mysql.php');
 include (MAIN_DIR . "/config.php");
+require __DIR__ . '/vendor/autoload.php';
+
+use Coinbase\Wallet\Resource\Account;
+use Coinbase\Wallet\Resource\Address;
+use Coinbase\Wallet\Enum\CurrencyCode;
+use Coinbase\Wallet\Resource\Transaction;
+use Coinbase\Wallet\Value\Money;
+use Coinbase\Wallet\Client;
+use Coinbase\Wallet\Configuration;
+
+
+
+if(!preg_match('/^[a-zA-Z0-9\._]{5,}$/', $_POST['nickname'])){
+	echo '{"error" : 1, "message": "Для регистрации в блокчейне не подходят любые символы, кроме латинских букв, цифр, точки и тире. Повторите ввод, пожалуйста.."}';
+	die();
+}
+
+if(!preg_match('/^.{12,70}$/', $_POST['pass'])){
+	echo '{"error" : 1, "message": "Мы заботимся о Вашей финансовой безопасности, не используйте пароль короче, чем 12 символов. Повторите ввод, пожалуйста.."}';
+	die();
+}
+
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+ 	echo '{"error" : 1, "message": "Проверьте корректность электронной почты, иначе, мы не сможем отправить Вам билеты. Повторите ввод, пожалуйста.."}';
+	die();   
+}
+
 
 
 $db = new SafeMysql(array('user' => $config['dbuser'], 'pass' => $config['dbpassword'],'db' => $config['dbname'],'host' => $config['dbhost'], 'charset' => 'utf8mb4'));
 
-$exist = $db->GetRow("SELECT * FROM mapalafest WHERE nickname=?s", $_POST['nickname']);
+$data['keys'] = get_keys($_POST['nickname'], $_POST['pass']);
 
+$exist = $db->GetRow("SELECT * FROM mapalafest WHERE nickname=?s", $_POST['nickname']);
+	
 if (!is_null($exist)){
-	echo '{"error" : "exist", "btc" : "btc_address"}';
-//	echo $exist['btc'];
+	if ($exist['keys']!=$data['keys']){
+		echo '{"error" : 1, "message" : "user exists"}';
+
+
+	} else {
+	echo '{"error" : 0, "btc" : "' . $exist['btc_address'] . '", "amount" : "' . $exist['amount_btc'] . '"}';
+	}
 
 } else {
 
-	$db->query("INSERT INTO mapalafest SET ?u", $_POST);
-	//here we recieve BTC-adress
-	echo '{"is_new" : 1, "btc" : "btc_address"}';
+	$data['nickname'] = $_POST['nickname'];
+	$data['kolvo'] = $_POST['kolvo'];
+	$data['email'] = $_POST['email'];
 
+	$btc = create_address($data['nickname']);
+	$data['btc_address'] = $btc;
+	$data['rate'] = get_btc_rate();
+	$data['amount_btc'] =  round($data['kolvo'] * 1000 / $data['rate'],5);
+
+	$db->query("INSERT INTO mapalafest SET ?u", $data);
+	
+	echo '{"error" : 0, "btc" : "' . $btc . '", "amount" : "' . $data['amount_btc'] . '"}';
+
+}
+
+
+
+
+
+
+function create_address($username){
+global $config;
+ 		$apiKey = $config['apiKey'];
+		$apiSecret = $config['apiSecret'];
+        $configuration = Configuration::apiKey($apiKey, $apiSecret);
+        $client = Client::create($configuration);
+        
+        $account = $client->getAccount($config['account']);
+               
+        $address = new Address([
+            'name' => $username,
+        ]);
+
+        $client->createAccountAddress($account, $address);
+        $address = $client->decodeLastResponse();
+     	
+     return $address['data']['address'];
+}
+
+ function get_btc_rate(){
+        
+        $jsnsrc = "https://blockchain.info/ticker";
+        $json = file_get_contents($jsnsrc);
+        $json = json_decode($json);
+        $btc_rate = $json->RUB->last;
+        
+        return $btc_rate;
+
+    }
+
+ function get_keys($nickname, $pass){
+        $cmd = "python3 pub_keys.py" . " DCM " . "'" . $nickname . "'" . " " . $pass;
+        $out = shell_exec($cmd);  
+        $out = json_decode($out, true);
+        return $out['active'];
 }
 
 
