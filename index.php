dcc77a07a150754cd2a2286464ef4bb6f52b316d
Dark Sun <dacom.dark.sun@gmail.com>
2017-08-24 11:41:39 +0500

    index

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter45721044 = new Ya.Metrika({
+                    id:45721044,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/45721044" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 
 
 <!DOCTYPE html>
@@ -19,7 +48,7 @@
 </head>
 
 <body>
-<script src='ya.js'></script>
+
 
 <header class="header">
 
